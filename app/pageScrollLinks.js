0590bc5145a70e421f0d5aade313ebefb86b4da7
Kamil Oleksiuk <koleksiuk20@gmail.com>
2016-06-02 16:34:37 +0200

    cleared pageScrollLinks javascript code

--- app/pageScrollLinks.js
+++ app/pageScrollLinks.js
@@ -2,12 +2,12 @@ function initPageScrollLinks() {
     $('.page-scroll').on('click', function(event) {
         event.preventDefault();
 
-        let target = $(event.target).attr('href'),
+        var target = $(event.target).attr('href'),
             elem = $(target),
             offset = elem.offset().top - 60;
 
         $('body, html').animate({scrollTop: offset + 1}, 500);
     });
-};
+}
 
 module.exports = initPageScrollLinks();
