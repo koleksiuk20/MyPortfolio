04a91bc68e5b93ffbeb3830159c7b2878c2d9219
Kamil Oleksiuk <koleksiuk20@gmail.com>
2016-06-09 09:44:20 +0200

    clean code in animatedProgressBar

--- app/animatedProgressBar.js
+++ app/animatedProgressBar.js
@@ -5,15 +5,13 @@ function animatedProgressBar() {
         $(window).scroll(function () {
             $('.animatedEntrance').each(function () {
                 var imagePos = $(this).offset().top;
-
                 var topOfWindow = $(window).scrollTop();
 
                 if (imagePos < topOfWindow + 60) {
-                    $('.progress-bar-value').css('visibility', 'visible').addClass('animated').addClass('slideInLeft');
-                    $('.progress-bar').css('visibility', 'visible').addClass('animated').addClass('slideInLeft');
+                    $('.progress-bar-value').css('visibility', 'visible').addClass('animated slideInLeft');
+                    $('.progress-bar').css('visibility', 'visible').addClass('animated slideInLeft');
                 }
-
-            })
+            });
         });
     }
 }
